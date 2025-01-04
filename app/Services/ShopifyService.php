bb0302530fbb7d4c22734bb43a1b6438eb7939b9
alicaglar <ali.caglar@std.yildiz.edu.tr>
2025-01-04 11:43:24 +0300

    feat: add shopify service and related thing

---
+++ app/Services/ShopifyService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+
+class ShopifyService
+{
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => config('services.shopify.store_url'),
+            'headers' => [
+                'X-Shopify-Access-Token' => config('services.shopify.access_token'),
+                'Content-Type' => 'application/json',
+            ],
+        ]);
+    }
+
+    public function query($query)
+    {
+        $response = $this->client->post('/admin/api/2025-01/graphql.json', [
+            'json' => [
+                'query' => $query
+            ],
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+}
--- config/services.php
+++ config/services.php
@@ -35,4 +35,9 @@ return [
         ],
     ],
 
+    'shopify' => [
+        'store_url' => env('SHOPIFY_STORE_URL'),
+        'access_token' => env('SHOPIFY_ACCESS_TOKEN'),
+    ],
+
 ];
